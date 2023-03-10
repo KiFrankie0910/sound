f357f59c9a63cda0d9798c4eaf3f28280c806546
Ki Frankie <106165688+kifrankie0910@users.noreply.github.com>
2023-03-10 15:41:19 +0800

    led

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+let sound = 0
 OLED.init(128, 64)
+let strip = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
 basic.forever(function () {
+    sound = smarthome.ReadNoise(AnalogPin.P2)
+    OLED.writeNumNewLine(sound)
     OLED.writeNumNewLine(smarthome.ReadNoise(AnalogPin.P2))
+    if (sound >= 60) {
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+    } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    }
 })
